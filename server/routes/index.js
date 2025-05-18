d4abc95fd6be7b46363db9821bb35a3d5bff5078
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2025-05-18 13:27:57 +0200

    Mis à jour de l'appel des routes - configureRoutes - dans le fichier index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
 const configureRoutes = (app) => {
-  app.use('/api/auth', require('./api/auth'));
-  app.use('/api/users', require('./api/users'));
-  app.use('/api/mails', require('./api/mails'));
-  app.use('/api/chips', require('./api/chips'));
+  app.use('/api/auth', require(path.join(__dirname, 'routes/api/auth')));
+  app.use('/api/users', require(path.join(__dirname, 'routes/api/users')));
+  app.use('/api/mails', require(path.join(__dirname, 'routes/api/mails')));
+  app.use('/api/chips', require(path.join(__dirname, 'routes/api/chips')));
+
+  // Default route
   app.use('/', (req, res) => {
     res.status(200).send('Welcome to Njambo!');
   });
